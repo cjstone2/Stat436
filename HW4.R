db731988066a97a644a1c5132b7f63a8169da99c
cjstone2 <145411323+cjstone2@users.noreply.github.com>
2024-12-09 22:25:46 -0600

    Update HW4.R

--- HW4.R
+++ HW4.R
@@ -3,7 +3,7 @@ library(plotly)
 library(dplyr)
 library(DT)
 
-data <- read.csv("Electric_Vehicle_Population_Data.csv") %>%
+data <- read.csv("https://www.dropbox.com/scl/fi/aarlsjjysy50zlkb49n6p/Electric_Vehicle_Population_Data.csv?rlkey=7osmeq4r0kualodwafprsdpma&st=0n25517o&dl=1") %>%
   filter(complete.cases(.)) %>%    
   filter(Base.MSRP > 0, Electric.Range > 0) %>% 
   distinct()     
@@ -11,7 +11,7 @@ data <- read.csv("Electric_Vehicle_Population_Data.csv") %>%
 data$Model.Year <- as.integer(data$Model.Year)
 
 ui <- fluidPage(
-  titlePanel("Electric Vehicle Finder"),
+  titlePanel("Electric and Hybrid Vehicle Finder"),
   
   sidebarLayout(
     sidebarPanel(
@@ -31,11 +31,14 @@ ui <- fluidPage(
                   min = min(data$Model.Year), max = max(data$Model.Year),
                   value = c(min(data$Model.Year), max(data$Model.Year)),
                   step = 1,
-                  sep = "")
+                  sep = ""),
+      selectInput("evTypeInput", "Electric Vehicle Type:",
+                  choices = unique(data$Electric.Vehicle.Type),
+                  selected = "Battery Electric Vehicle (BEV)",
+                  multiple = TRUE)
     ),
     mainPanel(
-      plotlyOutput("scatterPlot", width = "100%", height = "650px"),
-      DTOutput("carTable")
+      plotlyOutput("scatterPlot", width = "100%", height = "650px")
     )
   )
 )
@@ -51,7 +54,8 @@ server <- function(input, output) {
         Base.MSRP <= input$priceInput[2],
         Model.Year >= input$yearInput[1],
         Model.Year <= input$yearInput[2],
-        if (!is.null(input$makeInput)) Make %in% input$makeInput else TRUE
+        if (!is.null(input$makeInput)) Make %in% input$makeInput else TRUE,
+        Electric.Vehicle.Type == input$evTypeInput
       )
   })
   
@@ -67,14 +71,15 @@ server <- function(input, output) {
         "Model:", Model, "<br>",
         "Range:", Electric.Range, "miles<br>",
         "Price:", Base.MSRP, "<br>",
-        "Year:", Model.Year
+        "Year:", Model.Year, "<br>",
+        "Type:", Electric.Vehicle.Type
       ),
       hoverinfo = "text",
       type = "scatter3d",
       mode = "markers"
     ) %>%
       layout(
-        title = "Electric Vehicle Finder",
+        title = "Electric/Hybrid Vehicle Finder",
         scene = list(
           xaxis = list(title = "Vehicle Range (miles)"),
           yaxis = list(title = "Base MSRP ($)"),
@@ -82,7 +87,7 @@ server <- function(input, output) {
         )
       )
   })
-  
+}
 
 shinyApp(ui = ui, server = server)
 
